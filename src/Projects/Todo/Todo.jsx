eeec9485a24001bae2bef6d3ad3949b65cb9296f
sairaj730 <vadasairaju123@gmail.com>
2025-04-12 15:22:41 +0530

    updated changes

---
+++ src/Projects/Todo/Todo.jsx
@@ -0,0 +1,66 @@
+import { FaCalendarCheck } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import {useState} from "react";
+import "./Todo.css";
+export const Todo = () => {
+const [inputValue,setinputValue] = useState("");
+const [task,setTask]=useState([]);
+
+const handleInputChange = (val) => {
+    setinputValue(val);
+}
+
+const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    if(!inputValue) return;
+
+    if(task.includes(inputValue)) {
+        setinputValue("");
+        return;
+    }
+    
+    setTask((value) => [...value, inputValue]);
+    setinputValue("");
+}
+    return (
+        <section className="todo-container">
+            <header>
+                <h1>Todo List</h1>
+            </header>
+            <section className="form">
+                <form onSubmit={handleFormSubmit}>
+                    <div>
+                        <input
+                        type="text" 
+                        className="todo-input" 
+                        autoComplete="off" 
+                        value={inputValue}
+                        onChange={(e) => handleInputChange(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <button type="submit" className="todo-btn">
+                            Add task
+                        </button>
+                    </div>
+                </form>
+            </section>
+            <section className="myUnOrdList">
+                <ul className="todo-list">
+                    {
+                        task.map((curTask,idx) => {
+                            return (
+                                <li key={idx} className="todo-item">
+                                <span>{curTask}</span>
+                                <button className="check-btn"><FaCalendarCheck /></button>
+                                <button className="delete-btn"><MdDelete /></button>
+                            </li>
+                            );
+                        })
+                    }
+                </ul>
+            </section>
+        </section>
+    );
+}
